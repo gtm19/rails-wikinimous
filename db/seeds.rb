7c7d3030e51cb902498ce8a9f07fbc6423a69179
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-23 17:22:46 +0000

    added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new(
+    title: Faker::Quote.famous_last_words,
+    content: Faker::Movies::HitchhikersGuideToTheGalaxy.quote
+  )
+  article.save
+end
